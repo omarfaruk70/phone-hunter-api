67dc651a60a2af2b145de73ff4a290616d9a085c
omarfaruk70 <faruk1611281892@gmail.com>
2023-09-04 01:04:30 +0600

    project done

--- script/script.js
+++ script/script.js
@@ -14,7 +14,7 @@ const loadPhone = async (searchText,isShowAll) => {
 const displayphones = (phones, isShowAll) => {
     const cardContainer = document.getElementById('phoneContainer');
     cardContainer.textContent = '';
-    console.log(phones.length)
+    // console.log(phones.length)
     const showAllContainer = document.getElementById('showAll-container');
     // display show all button if there are more than 20 phones.
     if(phones.length > 12 && !isShowAll){
@@ -36,7 +36,7 @@ const displayphones = (phones, isShowAll) => {
           <h2 class="card-title">${phone.phone_name}</h2>
           <p>${phone.slug}</p>
           <div class="card-actions justify-center">
-            <button class="btn btn-primary">Buy Now</button>
+            <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
           </div>
         </div>
         `;
@@ -67,4 +67,35 @@ const toggleSpinner = (isLoading) =>{
 // handle show all Button
 const handleShowAll = () =>{
   handleSearch(true)
+}
+
+// handle show details
+const handleShowDetails = async (id) => {
+  // console.log('button clicked', id);
+  // load every single phone data
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  // console.log(data)
+  const phone = data.data
+  showPhoneDetails(phone)
+};
+
+const showPhoneDetails = (phoneInformation) =>{
+  // show the modal
+  console.log(phoneInformation);
+  const detailsInfoContainer = document.getElementById('details-information-container');
+  detailsInfoContainer.innerHTML = `
+  <img class='grid justify-center item-center' src="${phoneInformation?.image}" alt="">
+  <p class='font-semibold text-xl'> Name:  <span>${phoneInformation?.slug}</span></p>
+  <p class='font-semibold text-xl'> Brand:  <span>${phoneInformation?.brand}</span></p>
+  <p class='font-semibold text-xl'> ReleaseDate:  <span>${phoneInformation?.releaseDate}</span></p>
+  <p class='font-semibold text-xl'>Storage:  <span>${phoneInformation?.mainFeatures?.storage}</span></p>
+  <p class='font-semibold text-xl'>displaySize:  <span>${phoneInformation?.mainFeatures?.displaySize}</span></p>
+  <p class='font-semibold text-xl'> chipSet:  <span>${phoneInformation?.mainFeatures?.chipSet}</span></p>
+  <p class='font-semibold text-xl'> memory:  <span>${phoneInformation?.mainFeatures?.memory}</span></p>
+  <p class='font-semibold text-xl'> sensors:  <span>${phoneInformation?.mainFeatures?.sensors || 'No sensors this device'}</span></p>
+  
+  `
+  show_details_modal.showModal()
+
 }
